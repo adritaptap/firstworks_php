97b3921f7f9d0a5511692a8da53929076d011264
Adrien Juhem <adrien.juhem@gmail.com>
2016-12-05 23:13:23 +0100

    add news in blog files

--- bdd/blog/commentaires.php
+++ bdd/blog/commentaires.php
@@ -15,10 +15,11 @@
 <body>
 
 	<div class="container">
-		<p><a href="index.php" class="btn btn-success">Retour</a></p>
+		<p><a href="index.php" class="btn btn-default">Retour</a></p>
 		<?php
 
-		$numBlog = $_GET['numBlog'];
+		$numBlog = htmlspecialchars($_GET['numBlog']);
+
 // Connexion à la base de données
 		try
 		{
@@ -34,57 +35,63 @@
 
 		$req->execute(array('numBlog' => $numBlog));
 		$donnees = $req->fetch();
-
-		echo '<div class="well well-lg news">
-		<h4 class="media-heading text reviews">' . $donnees["titre"] . '</h4>
-		<div class="media-date text reviews list-inline">' . $donnees["date_creation_fr"] . '</div>
-		<p class="media-comment">' . $donnees["message"] . '</p>     
-	</div>';
-
-	$req->closeCursor();
-	?>
-	<h1>Commentaires</h1>
-
-	<form action="commentaires_post" method="post" class="form-horizontal">'
-		<div class="form-group">
-			<label for="message">Message
-				<input type="text" name="message" class="form-control"/>
-			</label>
-		</div>
-		<div class="form-group">
-			<label for="auteur">Nom
-				<input type="text" name="auteur" class="form-control"/>
-			</label>
-		</div>
-		<?php echo'<input type="hidden" name="id_blog" value="' . $numBlog . '"/>';  ?>
-		<div class="form-group">
-			<button type="submit" value="envoyer"  class="btn btn-sm btn-success text-uppercase"><span class="glyphicon glyphicon-share-alt"></span> Commenter !</button>
-		</div>
-	</form>
-
-
-
-
-
-	<div class="media-body news">
-
-
-
-		<?php
-
-		$req = $bdd->prepare('SELECT auteur, message, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM commentaire WHERE id_blog = :numBlog2 ORDER BY date_creation');
-
-		$req->execute(array('numBlog2' => $numBlog));
-
-		while ($donnees = $req->fetch()) {
-
-			echo '<div class="well well-lg">
-			<div class="media-date text reviews list-inline"><strong> ' . $donnees["auteur"] . ' </strong> ' . $donnees["date_creation_fr"] . '</div>
-			<p class="media-comment">' . $donnees["message"] . '</p>           
+		if (isset($donnees['ID'])) {
+			echo '<div class="well well-lg news">
+			<h4 class="media-heading text reviews">' . $donnees["titre"] . '</h4>
+			<div class="media-date text reviews list-inline">' . $donnees["date_creation_fr"] . '</div>
+			<p class="media-comment">' . $donnees["message"] . '</p>     
 		</div>';
+
+		$req->closeCursor();
+		?>
+		<h1>Commentaires</h1>
+
+		<form action="commentaires_post" method="post" class="form-horizontal">'
+			<div class="form-group">
+				<label for="message">Message
+					<input type="text" name="message" class="form-control"/>
+				</label>
+			</div>
+			<div class="form-group">
+				<label for="auteur">Nom
+					<input type="text" name="auteur" class="form-control"/>
+				</label>
+			</div>
+			<?php echo'<input type="hidden" name="id_blog" value="' . $numBlog . '"/>';  ?>
+			<div class="form-group">
+				<button type="submit" value="envoyer"  class="btn btn-sm btn-success text-uppercase"><span class="glyphicon glyphicon-share-alt"></span> Commenter !</button>
+			</div>
+		</form>
+
+
+
+
+
+		<div class="media-body news">
+
+
+
+			<?php
+
+			$req = $bdd->prepare('SELECT auteur, message, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM commentaire WHERE id_blog = :numBlog2 ORDER BY date_creation');
+
+			$req->execute(array('numBlog2' => $numBlog));
+
+			while ($donnees = $req->fetch()) {
+
+				echo '<div class="well well-lg">
+				<div class="media-date text reviews list-inline"><strong> ' . $donnees["auteur"] . ' </strong> ' . $donnees["date_creation_fr"] . '</div>
+				<p class="media-comment">' . $donnees["message"] . '</p>           
+			</div>';
+		}
+
+		$req->closeCursor();
+	}
+	else
+	{
+	echo "<h1>Erreur : Désolé mais ce commentaire n'existe pas !</h1>";
 	}
 
-	$req->closeCursor();
 
 	?>
 
